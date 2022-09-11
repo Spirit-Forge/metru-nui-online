99fb6d4f3234d6b8cdedcc9f6c26cf4316fda807
JrMasterModelBuilder <legojrmastermodelbuilder@gmail.com>
2022-09-11 15:18:27 -0400

    Default task

--- make.mjs
+++ make.mjs
@@ -89,7 +89,7 @@ async function browser(dest) {
 	));
 }
 
-const task = {};
+const task = {'': _ => Object.keys(task).map(t => t && console.error(t)) && 1};
 
 task['clean'] = async () => {
 	await remove('build', 'dist');
@@ -261,4 +261,4 @@ task['dist:linux-x86_64:tgz'] = async () => {
 	await makeTgz(`dist/${distName}-Linux-x86_64.tgz`, 'build/linux-x86_64');
 };
 
-await task[process.argv[2]]();
+process.exitCode = await task[process.argv[2] || '']();
